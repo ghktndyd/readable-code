b7a75d367170d6dbae1853aa49e329d3886df81a
ghktndyd <hwasuyong99@naver.com>
2024-09-11 15:37:28 +0900

    refactor: ISP - 인터페이스 분리 원칙에 따라서 특정 책임에 따라서 분리된 것이 맞는지 확인하고 리팩터링

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -6,7 +6,9 @@ import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
 public class GameApplication {
     public static void main(String[] args) {
         GameLevel gameLevel = new VeryBeginner();
+
         Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.initialize();
         minesweeper.run();
     }
 }
--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -1,24 +1,30 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.game.GameInitializable;
+import cleancode.minesweeper.tobe.game.GameRunnable;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 
-public class Minesweeper {
+public class Minesweeper implements GameInitializable, GameRunnable {
     private final GameBoard gameBoard;
     private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
     private final ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
     private final ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
+    private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
 
     public Minesweeper(GameLevel gameLevel) {
         gameBoard = new GameBoard(gameLevel);
     }
 
-    private int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
+    @Override
+    public void initialize() {
+        gameBoard.initializeGame();
+    }
 
+    @Override
     public void run() {
         consoleOutputHandler.showGameStartComments();
-        gameBoard.initializeGame();
 
         while (true) {
             try {
