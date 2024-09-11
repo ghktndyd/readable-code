9f49ea10b683e9586f28d95aa9f06b9ab9a89d80
ghktndyd <hwasuyong99@naver.com>
2024-09-11 20:10:43 +0900

    refactor: 일급 컬렉션을 사용해서 컬렉션도 추상화 레벨을 맞춰주기

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,13 +1,13 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
-import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
-        GameLevel gameLevel = new VeryBeginner();
+        GameLevel gameLevel = new Beginner();
         ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
         ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
 
