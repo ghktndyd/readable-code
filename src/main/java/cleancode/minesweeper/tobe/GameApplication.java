decf3df1af2259ef89d64bbcb2dccc4c23f584d4
ghktndyd <hwasuyong99@naver.com>
2024-09-12 13:21:18 +0900

    refactor: GameConfig를 만들어서 게임 설정을 하나의 클래스에서 관리하기

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,17 +1,16 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
-import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
-        GameLevel gameLevel = new Beginner();
-        ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
-        ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel, consoleInputHandler, consoleOutputHandler);
+        GameConfig gameConfig = new GameConfig(new Beginner(), new ConsoleInputHandler(), new ConsoleOutputHandler());
+
+        Minesweeper minesweeper = new Minesweeper(gameConfig);
         minesweeper.initialize();
         minesweeper.run();
     }
